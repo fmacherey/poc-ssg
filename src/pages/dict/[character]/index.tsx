b4f589da8458b4125d8df8ca86850419ea0d3b03
Florian Macherey <florian.macherey@wetter.com>
2024-03-12 17:22:38 +0100

    refine POC

--- src/pages/dict/[character]/index.tsx
+++ src/pages/dict/[character]/index.tsx
@@ -3,36 +3,56 @@ import React from "react";
 import NextLink from "next/link";
 
 export const Character: React.FC = () => {
-    const {query} = useRouter();
+  const { query } = useRouter();
 
-    return (
-        <div>
-            <div>
-                {`Character ${JSON.stringify(query)}`}
-            </div>
-            <div>
-                Links ssg: <NextLink href={"/lexikon/a"}>a</NextLink> - <NextLink href={"/lexikon/b"}>b</NextLink> - <NextLink href={"/lexikon/c"}>c</NextLink>
-            </div>
-            <div>
-                Links not ssg: <NextLink href={"/lexikon/d"}>d</NextLink> - <NextLink href={"/lexikon/e"}>e</NextLink> - <NextLink href={"/lexikon/f"}>f</NextLink>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>{`Character ${JSON.stringify(query)}`}</div>
+      <div>
+        Links ssg:{" "}
+        <NextLink href={"/dict/[character]?character=a"} as={"/lexikon/a"}>
+          a
+        </NextLink>{" "}
+        -{" "}
+        <NextLink href={"/dict/[character]?character=b"} as={"/lexikon/b"}>
+          b
+        </NextLink>{" "}
+        -{" "}
+        <NextLink href={"/dict/[character]?character=c"} as={"/lexikon/c"}>
+          c
+        </NextLink>
+      </div>
+      <div>
+        Links not ssg:{" "}
+        <NextLink href={"/dict/[character]?character=d"} as={"/lexikon/d"}>
+          d
+        </NextLink>{" "}
+        -{" "}
+        <NextLink href={"/dict/[character]?character=e"} as={"/lexikon/e"}>
+          e
+        </NextLink>{" "}
+        -{" "}
+        <NextLink href={"/dict/[character]?character=e"} as={"/lexikon/f"}>
+          f
+        </NextLink>
+      </div>
+    </div>
+  );
 };
 
 export const getStaticPaths = async () => {
-    const characters = ["a", "b", "c"];
+  const characters = ["a", "b", "c"];
 
-    return {
-        paths: characters.map(character => ({ params: { character }})),
-        fallback: true,
-    };
-}
+  return {
+    paths: characters.map((character) => ({ params: { character } })),
+    fallback: true,
+  };
+};
 
 export const getStaticProps = async () => {
-    return {
-        props: {},
-    };
-}
+  return {
+    props: {},
+  };
+};
 
 export default Character;
